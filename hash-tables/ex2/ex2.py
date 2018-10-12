0262f147c6344d0a673bc6a2d34a960c5a87a844
Simon <Simon_Freelance@hotmail.com>
2018-10-12 11:38:07 -0400

    Exercise 2 completed and complies with tests

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,5 +1,25 @@
 def reconstruct_trip(tickets):
-  pass 
+  
+  places = {}
+
+  for i in range(len(tickets)):
+    source = tickets[i][0]
+    destination = tickets[i][1]
+    places[source] = destination
+  
+  source = None
+  trip = []
+
+  for i in range(len(tickets)):
+    if source in places:
+      if places[source] is not None:
+        trip.append(places[source])
+      source = places[source]
+
+    else:
+      return []
+  
+  return trip
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
