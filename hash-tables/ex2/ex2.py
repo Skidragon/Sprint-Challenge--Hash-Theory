96972b32aca8a44015093a53df823bab3f99658e
Simon <Simon_Freelance@hotmail.com>
2018-10-12 14:54:32 -0400

    Refactored exercise 2 to use dict comprehension

--- hash-tables/ex2/ex2.py
+++ hash-tables/ex2/ex2.py
@@ -1,15 +1,10 @@
 def reconstruct_trip(tickets):
-  
-  places = {}
 
-  for i in range(len(tickets)):
-    source = tickets[i][0]
-    destination = tickets[i][1]
-    places[source] = destination
-  
+  places = dict(tickets)
+
   source = None
   trip = []
-
+  
   for i in range(len(tickets)):
     if source in places:
       if places[source] is not None:
